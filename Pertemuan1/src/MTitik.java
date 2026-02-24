44d4e15020fcf6f7ceba8cf5c34a6dce07b7c0d3
An Al Rivaldi <analrivaldi@gmail.com>
2026-02-24 13:47:01 +0700

    Pertemuan 1 selesai

--- Pertemuan1/src/MTitik.java
+++ Pertemuan1/src/MTitik.java
@@ -1,7 +1,8 @@
-// Nama file : MTitik.java
-// Deskripsi : berisi atribut dan method dalam class Titik
-// Pembuat : 24060124120030 - An Al Rivaldi
-// Tanggal : 22 Februari 2026
+/* Nama file    : MTitik.java
+ * Deskripsi    : berisi atribut dan method dalam class Titik.
+ * Pembuat      : 24060124120030 - An Al Rivaldi
+ * Tanggal      : 22 Februari 2026
+ */
 public class MTitik {
     public static void main(String[] args) {
         Titik T1 = new Titik();
