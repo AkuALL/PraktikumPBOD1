21986736c5391a77b6ca9f3dc74ef7ea251b9599
An Al Rivaldi <analrivaldi@gmail.com>
2026-02-25 23:04:20 +0700

    Pertemuan 2 selesai

--- Pertemuan2/src/Garis.java
+++ Pertemuan2/src/Garis.java
@@ -6,67 +6,67 @@
 
 public class Garis {
     /**************** ATRIBUT ****************/
-    Titik titikAwal;
-    Titik titikAkhir;
-    static int counterGaris = 0;
+    private Titik titikAwal;
+    private Titik titikAkhir;
+    private static int counterGaris = 0;
 
     /**************** METHOD ****************/
     // Konstruktor untuk membuat garis dengan Titik X dan Titik Y tertentu
-    Garis(Titik X, Titik Y) {
+    public Garis(Titik X, Titik Y) {
         this.titikAwal = X;
         this.titikAkhir = Y;
         counterGaris++;
     }
 
     // Konstruktor untuk membuat garis dengan Titik X(0,0) dan Titik Y(1,1)
-    Garis() {
+    public Garis() {
         titikAwal = new Titik();
         titikAkhir = new Titik(1,1);
         counterGaris++;
     }
 
     // Mengembalikan Titik Awal dari garis
-    Titik getTitikAwal() {
+    public Titik getTitikAwal() {
         return titikAwal;
     }
 
     // Mengembalikan Titik Akhir dari garis
-    Titik getTitikAkhir() {
+    public Titik getTitikAkhir() {
         return titikAkhir;
     }
 
     // Mengembalikan nilai counterGaris
-    static int getCounterGaris() {
+    public static int getCounterGaris() {
         return counterGaris;
     }
 
     // Mengeset Titik Awal garis dengan nilai baru X
-    void setTitikAwal(Titik X) {
+    public void setTitikAwal(Titik X) {
         titikAwal = X;
     }
 
     // Mengeset Titik Akhir garis dengan nilai baru Y
-    void setTitikAkhir(Titik Y) {
+    public void setTitikAkhir(Titik Y) {
         titikAkhir = Y;
     }
 
     // Mengembalikan nilai dari panjang sebuah garis
-    double getPanjangGaris() {
+    public double getPanjangGaris() {
         return Math.sqrt(Math.pow(titikAkhir.getAbsis() - titikAwal.getAbsis(), 2) + Math.pow(titikAkhir.getOrdinat() - titikAwal.getOrdinat(), 2));
     }
 
     // Mengembalikan nilai dari gradien sebuah garis
-    double getGradienGaris() {
+    public double getGradienGaris() {
         return (titikAkhir.getOrdinat() - titikAwal.getOrdinat()) / (titikAkhir.getAbsis() - titikAwal.getAbsis());
     }
 
     // Mengembalikan koordinat titik tengah dari sebuah garis
-    Titik getMidPointGaris() {
+    public Titik getMidPointGaris() {
         return new Titik((titikAkhir.getAbsis() + titikAwal.getAbsis())/2, (titikAkhir.getOrdinat()+titikAwal.getOrdinat())/2);
     }
 
     // Mengembalikan nilai true jika dua buah garis sejajar
-    boolean isGarisSejajar(Garis G1) {
+    public boolean isGarisSejajar(Garis G1) {
         if (G1.getGradienGaris() == this.getGradienGaris()) {
             return true;
         } else {
@@ -75,7 +75,7 @@ public class Garis {
     }
 
     // Mengembalikan nilai true jika dua buah garis tegak lurus
-    boolean isGarisTegakLurus(Garis G1) {
+    public boolean isGarisTegakLurus(Garis G1) {
         if (G1.getGradienGaris() * this.getGradienGaris() == -1) {
             return true;
         } else {
@@ -84,12 +84,12 @@ public class Garis {
     }
 
     // Menampilkan titik awal dan titik akhir dari garis
-    void printGaris() {
+    public void printGaris() {
         System.out.println("Titik Awal (" + titikAwal.getAbsis() + ", " + titikAwal.getOrdinat() + "), Titik Akhir (" + titikAkhir.getAbsis() + ", " + titikAkhir.getOrdinat() + ")");    
     }
 
     // Menampilkan persamaan garis dalam bentuk (y = mx + c)
-    void printPersamaanGaris() {
+    public void printPersamaanGaris() {
         System.out.println("y = " + getGradienGaris() + "x" + " + " + (titikAkhir.getOrdinat() - getGradienGaris()*titikAkhir.getAbsis()));
     }
 } // End Class Garis
--- Pertemuan2/src/Titik.java
+++ Pertemuan2/src/Titik.java
@@ -6,55 +6,56 @@
 
 public class Titik {
     /**************** ATRIBUT ****************/
-    double absis;
-    double ordinat;
-    static int counterTitik = 0;
+    private double absis;
+    private double ordinat;
+    private static int counterTitik = 0;
 
     /**************** METHOD ****************/
     // Konstruktor untuk membuat titik dengan nilai absis dan ordinat tertentu
-    Titik(double absis, double ordinat) {
+    public Titik(double absis, double ordinat) {
         this.absis = absis;
         this.ordinat = ordinat;
         counterTitik++;
     }
 
     // Konstruktor untuk membuat titik (0, 0)
-    Titik() {
+    public Titik() {
         this(0, 0);
     }
 
     // Mengembalikan nilai counterTitik
-    static int getCounterTitik() {
+    public static int getCounterTitik() {
         return counterTitik;
     }
 
     // Mengembalikan nilai absis
-    double getAbsis() {
+    public double getAbsis() {
         return absis;
     }
 
     // Mengembalikan nilai ordinat
-    double getOrdinat() {
+    public double getOrdinat() {
         return ordinat;
     }
 
     // Mengeset absis titik dengan nilai baru x
-    void setAbsis(double x) {
+    public void setAbsis(double x) {
         absis = x;
     }
 
     // Mengeset ordinat titik dengan nilai baru y
-    void setOrdinat(double y) {
+    public void setOrdinat(double y) {
         ordinat = y;
     }
 
     // Menggeser nilai absis dan ordinat titik masing-masing sejauh x dan y
-    void geser(double x, double y) {
+    public void geser(double x, double y) {
         absis = absis + x;
         ordinat = ordinat + y;
     }
 
-    int getKuadran() {
+    // Mengembalikan nilai kuadran dari titik
+    public int getKuadran() {
         if (this.absis > 0 && this.ordinat > 0) {
             return 1;
         } else if (this.absis < 0 && this.ordinat > 0) {
@@ -69,41 +70,41 @@ public class Titik {
     }
 
     // Menghitung jarak titik terhadap titik (0, 0)
-    double getJarakPusat() {
+    public double getJarakPusat() {
         return (double) Math.sqrt(Math.pow(this.absis, 2) + Math.pow(this.ordinat,2));
     }
 
     // Menghitung jarak titik T2 terhadap titik
-    double getJarak(Titik T2){
+    public double getJarak(Titik T2){
         return Math.sqrt(Math.pow(T2.getAbsis() - this.absis, 2) + Math.pow(T2.getOrdinat() - this.ordinat, 2));    }
 
     // Melakukan refleksi terhadap sumbu X
-    void refleksiX() {
+    public void refleksiX() {
         this.ordinat = this.ordinat * (-1);
     }
 
     // Melakukan refleksi terhadap sumbu Y
-    void refleksiY() {
+    public void refleksiY() {
         this.absis = this.absis * (-1);
     }
 
     // Menghasilkan sebuah titik baru yang merupakan hasil refleksi sumbu X dari sebuah objek titik
-    Titik getRefleksiX() {
+    public Titik getRefleksiX() {
         return new Titik(absis, ordinat * (-1));
     }
 
     // Menghasilkan sebuah titik baru yang merupakan hasil refleksi sumbu Y dari sebuah objek titik
-    Titik getRefleksiY() {
+    public Titik getRefleksiY() {
         return new Titik(absis*(-1), ordinat);
     }
 
     // Mencetak koordinat titik
-    void printTitik() {
+    public void printTitik() {
         System.out.println("Titik (" + absis + ", " + ordinat + ")");
     }
 
     // Mencetak nilai counterTitik (WARNING counterTitik merupakan atribut statis)
-    // void printCounterTitik() {
+    // public void printCounterTitik() {
     //     System.out.println(this.counterTitik);
     // }
 } // end of class Titik
