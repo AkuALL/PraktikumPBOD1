b024952f28a7f6602d046ad10a2066f87b95ba54
An Al Rivaldi <analrivaldi@gmail.com>
2026-02-25 15:32:32 +0700

    Pertemuan 2 Ongoing_part3_(Class Garis Ongoing)

--- Pertemuan2/src/Garis.java
+++ Pertemuan2/src/Garis.java
@@ -5,31 +5,50 @@
  */
 public class Garis {
     /**************** ATRIBUT ****************/
-    Titik X;
-    Titik Y;
+    Titik titikAwal;
+    Titik titikAkhir;
     static int counterGaris = 0;
 
     /**************** METHOD ****************/
     // Konstruktor untuk membuat garis dengan Titik X dan Titik Y tertentu
     Garis(Titik X, Titik Y) {
-        this.X = X;
-        this.Y = Y;
+        this.titikAwal = X;
+        this.titikAkhir = Y;
         counterGaris++;
     }
 
     // Konstruktor untuk membuat garis dengan Titik X(0,0) dan Titik Y(1,1)
     Garis() {
-        X = new Titik();
-        Y = new Titik(1,1);
+        titikAwal = new Titik();
+        titikAkhir = new Titik(1,1);
         counterGaris++;
+    }
+
+    Titik getTitikAwal() {
+        return titikAwal;
+    }
+
+    Titik getTitikAkhir() {
+        return titikAkhir;
     }
 
     static int getCounterGaris() {
         return counterGaris;
     }
 
-    
+    void setTitikAwal(Titik X) {
+        titikAwal = X;
+    }
 
+    void setTitikAkhir(Titik Y) {
+        titikAkhir = Y;
+    }
 
+    double getPanjangGaris() {
+        return Math.sqrt(Math.pow(titikAkhir.getAbsis() - titikAwal.getAbsis(), 2) + Math.pow(titikAkhir.getOrdinat() - titikAwal.getOrdinat(), 2));
+    }
 
+    double getGradienGaris() {
+        return (titikAkhir.getOrdinat() - titikAwal.getOrdinat()) / (titikAkhir.getAbsis() - titikAwal.getAbsis());
+    }
 }
