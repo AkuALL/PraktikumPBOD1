fd36ceace9138ee5540a6606140afc9be912759f
Al Rivaldi <analrivaldi@gmail.com>
2026-03-17 21:52:56 +0700

    Pertemuan 5 belum Selesai

--- Pertemuan5/src/Latihan/Manusia.java
+++ Pertemuan5/src/Latihan/Manusia.java
@@ -3,9 +3,62 @@ package Latihan;
 import java.time.LocalDate;
 
 public abstract class Manusia {
+    // ======================= ATRIBUT =======================
     protected String nama;
     protected LocalDate tgl_mulai_kerja;
     protected String alamat;
     protected double pendapatan;
     protected static int counterMns = 0;
+
+    // ======================= METHOD =======================
+    public Manusia(){
+        counterMns++;
+    }
+
+    public Manusia(String nama, LocalDate tgl_mulai_kerja, String alamat, double pendapatan){
+        this.nama = nama;
+        this.tgl_mulai_kerja = tgl_mulai_kerja;
+        this.alamat = alamat;
+        this.pendapatan = pendapatan;
+        counterMns++;
+    }
+
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+
+    public void setTgl_mulai_kerja(LocalDate tgl_mulai_kerja){
+        this.tgl_mulai_kerja = tgl_mulai_kerja;
+    }
+
+    public void setAlamat(String alamat){
+        this.alamat = alamat;
+    }
+
+    public void setPendapatan(double pendapatan){
+        this.pendapatan = pendapatan;
+    }
+
+    public String getNama(){
+        return nama;
+    }
+
+    public LocalDate getTgl_mulai_kerja(){
+        return tgl_mulai_kerja;
+    }
+
+    public String getAlamat(){
+        return alamat;
+    }
+
+    public double getPendapatan(){
+        return pendapatan;
+    }
+
+    public void cetakInfo(){
+        
+    }
+
+    public abstract int hitungMasaKerja();
+
 }
--- Pertemuan5/src/Latihan/PNS.java
+++ Pertemuan5/src/Latihan/PNS.java
@@ -1,5 +1,18 @@
 package Latihan;
 
 public class PNS {
-    
+    private String nip;
+    private static int counterPNS = 0;
+
+    public void setNIP(String nip){
+        this.nip = nip;
+    }
+
+    public String getNIP(){
+        return nip;
+    }
+
+    public int getCounterPNS(){
+        return counterPNS;
+    }
 }
